1b212cebbc5022146551783ad99ffaa46aab572b
wissehes <31771656+wissehes@users.noreply.github.com>
2019-09-20 08:01:30 +0200

    you can now suggest songs :)

--- chickenfm.js
+++ chickenfm.js
@@ -1,5 +1,27 @@
-function changeImage(imgName)
-{
-   image = document.getElementById('coverimg');
-   image.src = imgName;
+
+
+function sendMessage(){
+   msg = document.getElementById("message").value;
+   console.log(msg.value);
+   name = document.getElementById("name").value;
+
+   if(msg == "" || name == ""){
+      document.getElementById("submiterror").innerHTML = "<p style='text-align:center;'>You need to enter a name and message!</p>";
+      document.getElementById("submiterror").style = "background-color: red;";
+      exit(0)
+   }
+ 
+ var url = "https://wissehes.nl/chickenfm.php";
+ var params = "name="+name+"&msg="+msg;
+ var xhr = new XMLHttpRequest();
+ xhr.open("POST", url, true);
+
+ //Send the proper header information along with the request
+ xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+
+ xhr.send(params);
+
+ document.getElementById("submitreq").innerHTML = "<p style='text-align:center;'>success!</p>";
+ document.getElementById("submiterror").innerHTML = "";
+
 }
