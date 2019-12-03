e926c083ebf203c1abb7f98e274cddfd11b4bd2b
TheChicken14 <wissehes@gmail.com>
2019-12-03 18:31:48 +0100

    oh god

--- chickenfm.js
+++ chickenfm.js
@@ -1,4 +1,5 @@
 //https://stackoverflow.com/questions/9975810/make-iframe-automatically-adjust-height-according-to-the-contents-without-using
 function resizeIframe(obj) {
    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+   obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
  }
