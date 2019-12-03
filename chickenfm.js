01e51b935022c85ae88535a8535a41dd0d799bfa
TheChicken14 <wissehes@gmail.com>
2019-12-03 18:48:33 +0100

    ok

--- chickenfm.js
+++ chickenfm.js
@@ -1,5 +1,7 @@
 //https://stackoverflow.com/questions/9975810/make-iframe-automatically-adjust-height-according-to-the-contents-without-using
+var frameObj
 function resizeIframe(obj) {
+  frameObj = obj
    obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
    obj.style.width = obj.contentWindow.document.body.scrollWidth + 'px';
  }
--- scripts/popups/message.js
+++ scripts/popups/message.js
@@ -12,7 +12,14 @@ function windowOnClick(event) {
         toggleMsgModal();
     }
 }
-
+var listener = addEventListener('blur', function() {
+	if(document.activeElement === document.getElementById('messageFrame')) {
+        console.log('e')
+		setTimeout(function(){
+            resizeIframe(frameObj)
+        }, 3000)
+	}
+});
 msgtrigger.addEventListener("click", toggleMsgModal);
 msgcloseButton.addEventListener("click", toggleMsgModal);
 window.addEventListener("click", windowOnClick);
