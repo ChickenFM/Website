b4361c3907d3031ffb8899bc87fd1e15edeb8fb1
TheChicken14 <wissehes@gmail.com>
2019-12-03 18:29:58 +0100

    hey

--- chickenfm.js
+++ chickenfm.js
@@ -1,37 +1,4 @@
-function sendMessage() {
-   $("#my-form").submit(function(e) {
-      e.preventDefault();
-    
-      var $form = $(this);
-      $.post($form.attr("action"), $form.serialize()).then(function() {
-        alert("Thank you!");
-      });
-    });
-}
-
-/*function sendMessage(){
-   var msg = document.getElementById("message").value;
-   console.log(msg.value);
-   var name = document.getElementById("name").value;
-
-   if(msg == "" || name == ""){
-      document.getElementById("submiterror").innerHTML = "<p style='text-align:center;'>You need to enter a name and message!</p>";
-      document.getElementById("submiterror").style = "background-color: red;";
-      
-   }
- if(msg !== "" || name !== ""){
- var url = "https://wissehes.nl/chickenfm.php";
- var params = "name="+name+"&msg="+msg;
- var xhr = new XMLHttpRequest();
- xhr.open("POST", url, true);
-
- //Send the proper header information along with the request
- xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-
- xhr.send(params);
-
- document.getElementById("submitreq").innerHTML = "<p style='text-align:center;'>success!</p>";
- document.getElementById("submiterror").innerHTML = "";
-
-}
-}*/
+//https://stackoverflow.com/questions/9975810/make-iframe-automatically-adjust-height-according-to-the-contents-without-using
+function resizeIframe(obj) {
+   obj.style.height = obj.contentWindow.document.body.scrollHeight + 'px';
+ }
